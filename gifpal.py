4d6ad9e5a949e6131e4bcff5152bda26c600a956
John Herndon <jherndon@surfline.com>
2016-10-16 03:32:14 -0700

    Using argparser instead of argv. Added some additional CLI switches and related functionality (--version, --verbose, --show, --inverse). Added a nice header and footer.

--- gifpal.py
+++ gifpal.py
@@ -2,18 +2,57 @@
 
 # GIFpal by John Herndon
 
+import argparse
 import csv
 import sys
 from PIL import Image
 
-version = '1.1.0'
+version = '1.2.0'
+
+
+def verbose(verbosity, message):
+    if verbosity:
+        print message
+
+
+output_header = '''\
+ ______ _____ _______  _____  _______
+|  ____   |   |______ |_____] |_____| |
+|_____| __|__ |       |       |     | |_____ %s
+
+Convert a GIF image\'s palette to one of your choosing.
+''' % version
+
+output_footer = '''\
+
+examples:
+
+gifpal.py weather.gif falsecolor5.pal
+
+gifpal.py weather.gif falsecolor5.pal output.gif
+
+\000
+'''
 
 # Command line arguments
-if len(sys.argv) < 3:
-    print '  ______ _____ _______  _____  _______'
-    print ' |  ____   |   |______ |_____] |_____| |'
-    print ' |_____| __|__ |       |       |     | |_____ v%s\n' % version
-    sys.exit('Usage: %s <input GIF filename> <palette filename> [output GIF filename]' % sys.argv[0])
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=output_header, epilog=output_footer)
+
+parser.add_argument('in_file', help='Filename of GIF to be changed.', nargs=1)
+parser.add_argument('palette_file', help='Filename of 256-color CSV file containing the palette to use.', nargs=1)
+parser.add_argument('out_file', help='Filename of resulting GIF. (optional)', nargs='?')
+parser.add_argument('--inverse', action='store_true',
+                    help='Inverse palette application. (not recommended)')
+parser.add_argument('--show', action='store_true',
+                    help='Show resulting image.')
+parser.add_argument('--verbose', action='store_true',
+                    help='Step by step processing report.')
+parser.add_argument('--version', action='store_true', help='Output current version number and exit. ')
+
+args = parser.parse_args()
+
+if args.version:
+    sys.exit(output_header)
 
 # Two dimension array for palette
 num_colors, values = 256, 3
@@ -24,23 +63,52 @@ red = 0
 green = 1
 blue = 2
 
+verbose(args.verbose, output_header)
+
+verbose(args.verbose, 'Verbose output requested. (--verbose)')
+
 # Load palette (csv) file
-with open(sys.argv[2]) as f:
+with open(''.join(args.palette_file)) as f:
     reader = csv.reader(f)
-    x = 0
+    if args.inverse:
+        verbose(args.verbose, 'Inverse palette application requested. (--inverse)')
+        index = num_colors - 1
+    else:
+        index = 0
+
+    verbose(args.verbose, 'Loading palette file %s.' % ''.join(args.palette_file))
+
     for row in reader:
-        palette[x + red] = int(row[red])
-        palette[x + green] = int(row[green])
-        palette[x + blue] = int(row[blue])
-        x += 3
+        palette[index + red] = int(row[red])
+        palette[index + green] = int(row[green])
+        palette[index + blue] = int(row[blue])
+        if args.inverse:
+            index -= 3
+        else:
+            index += 3
+
 
 # Load GIF image into memory
-img = Image.open(sys.argv[1], "r")
+img = Image.open(''.join(args.in_file), "r")
+
+verbose(args.verbose, 'Loading GIF file %s.' % ''.join(args.in_file))
 
 # If it is indeed a gif, change the palette
 if img.tile[0][0] == "gif":
+    verbose(args.verbose, 'Applying new palette to GIF file %s.' % ''.join(args.in_file))
     img.putpalette(palette)
-    if len(sys.argv) == 4:
-        img.save(sys.argv[3])
+
+    if args.out_file:
+        verbose(args.verbose, 'Saving new GIF file %s.' % ''.join(args.out_file))
+        img.save(''.join(args.out_file))
     else:
-        img.save(sys.argv[1] + '_falsecolor.gif')
+        verbose(args.verbose, 'Saving new GIF file %s.' % ''.join(args.in_file) + '_falsecolor.gif')
+        img.save(''.join(args.in_file) + '_falsecolor.gif')
+
+    if args.show:
+        verbose(args.verbose, 'Displaying image. (--show)')
+        img.show()
+
+verbose(args.verbose, 'Process complete.')
+
+sys.exit(0)
